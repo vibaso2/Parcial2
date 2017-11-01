3f349454bf21c54aba286f4742abda63715dea4f
vibaso2 <vibasom@gmail.com>
2017-10-31 20:07:34 -0500

    elimine el comment que era el logout

--- login.php
+++ login.php
@@ -7,12 +7,6 @@ include('validate_log.php');
         <title>Login</title>
     </head>
     <body>
-	<!--
-       <?php if($_SESSION['username']): ?>
-            <p>You are logged in as <?=$_SESSION['username']?></p>
-            <p><a href="?logout=1">Logout</a></p>
-        <?php endif; ?> 
-	-->
         <form name="login" action="" method="post">
             Username:  <input type="text" name="username" value="" /><br />
             Password:  <input type="password" name="password" value="" /><br />
