356dfd8934f2550162cba57b00e9807986483a30
ladypikachu <33270620+ladypikachu@users.noreply.github.com>
2017-10-31 20:09:41 -0500

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,125 @@
 <?php
-include('validate_log.php');
+include ('validate_log.php');
 ?>
+
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <title>Login</title>
-    </head>
-    <body>
-        <form name="login" action="" method="post">
-            Username:  <input type="text" name="username" value="" /><br />
-            Password:  <input type="password" name="password" value="" /><br />
-            <input type="submit" name="submit" value="Submit" />
-        </form>
-    </body>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+
+    <title>Login - Banco ABC</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/freelancer.min.css" rel="stylesheet">
+    <link href="css/estilo.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="index.html">BANCO ABC</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <img class="img-fluid" src="img/profile.png">
+        <div class="intro-text">
+          <span class="name">Login de Usuario</span>
+          <hr class="star-light">
+        </div>
+      </div>
+    </header> 
+<div class="out">
+    <div class="logform">
+      <br>
+      <form action="" method="post">
+              Username:  <input type="text" name="username" value="" /><br />
+              Password:  <input type="password" name="password" value="" /><br /> <br>
+              <input type="submit" name="submit" value="Enviar" />
+            </form>
+            
+    </div>
+<br><br>
+</div>
+
+   
+   <footer class="text-center">
+      <div class="footer-above">
+        <div class="container">
+          <div class="row">
+            <div class="footer-col col-md-4">
+              <h3></h3>
+              <p>
+                <br></p>
+            </div>
+            <div class="footer-col col-md-4">
+              <h3>Banco ABC</h3>
+
+              <ul class="list-inline">
+                <li class="list-inline-item">
+                  <a class="btn-social btn-outline" href="#">
+                    <i class="fa fa-fw fa-facebook"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-social btn-outline" href="#">
+                    <i class="fa fa-fw fa-google-plus"></i>
+                  </a>
+                </li>
+                <li class="list-inline-item">
+                  <a class="btn-social btn-outline" href="#">
+                    <i class="fa fa-fw fa-twitter"></i>
+                  </a>
+                </li>
+                
+                
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="footer-below">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              Copyright &copy; <br>
+              Creado por: Victor Baso y Laura Centella 2017
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+
+  </body>
+
 </html>
